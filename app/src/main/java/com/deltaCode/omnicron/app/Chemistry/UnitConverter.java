25e548de9e57dc8c0c168924c6012fe9ff34161f
shaankewal <shaankewal@yahoo.com>
2015-10-26 13:32:26 -0400

    temp gas law stuff

--- app/src/main/java/com/deltaCode/omnicron/app/Chemistry/UnitConverter.java
+++ app/src/main/java/com/deltaCode/omnicron/app/Chemistry/UnitConverter.java
@@ -2,24 +2,23 @@ package com.deltaCode.omnicron.app.Chemistry;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import com.deltaCode.omnicron.app.R;
 
-public class hahaha extends AppCompatActivity {
+public class UnitConverter extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hahaha);
+        setContentView(R.layout.activity_unit_converter);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_hahaha, menu);
+        getMenuInflater().inflate(R.menu.menu_unit_converter, menu);
         return true;
     }
 
